0c8224e61b2a9e8f45aa52d147f3b83e728778ef
Alexander Solodov <85291734+fuutuuroo@users.noreply.github.com>
2023-06-20 00:51:47 +0500

    Add files via upload

---
+++ OOP_LabRab10/Pair.h
@@ -0,0 +1,54 @@
+﻿#pragma once
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+class Pair {
+private:
+	int left;
+	double right;
+public:
+	//конструкторы
+	Pair() {
+		left = 0;
+		right = 0.0;
+	}
+	Pair(int l, double r) {
+		left = l;
+		right = r;
+	}
+	Pair(const Pair& p) {
+		left = p.left;
+		right = p.right;
+	}
+	~Pair() {}
+	//геттеры
+	int get_left() {
+		return left;
+	}
+	void set_left(int l) {
+		left = l;
+	}
+	//сеттеры
+	double get_right() {
+		return right;
+	}
+	void set_right(double r) {
+		right = r;
+	}
+	//перегрузки операторов сравнения
+	void operator < (const Pair& p);
+	void operator > (const Pair& p);
+	//перегрузка префиксного инкремента
+	void operator ++ () { ++left; }
+	//перегрузка постфиксного инкремента
+	void operator ++ (int) { right += 1.0; }
+	//перегрузка оператора сложения
+	Pair operator + (int); 
+	//перегрузки операций ввода-вывода
+	friend istream& operator >> (istream&, Pair&);
+	friend ostream& operator << (ostream&, const Pair&);
+	//перегрузки для работы с файловым потоком
+	friend fstream& operator >> (fstream&, Pair&);
+	friend fstream& operator << (fstream&, const Pair&);
+};
---
+++ OOP_LabRab10/file_work.h
@@ -0,0 +1,103 @@
+﻿#include "Pair.h"
+#include <string>
+
+int make_file(const char* f_name)
+{
+	fstream stream(f_name, ios::out | ios::trunc); //Открыть для записи
+	if (!stream)
+		return -1; //Ошибка открытия файла
+	int n;
+	Pair p;
+	cout << "Введите количество экземпляров: "; 
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> p; //Ввод атрибутов объекта из стандартного потока
+		stream << p << "\n"; //Запись объекта в файловый поток
+	}
+	stream.close(); //Закрыть поток
+	return n; //Вернуть количество записанных объектов
+}
+int print_file(const char* f_name)
+{
+	fstream stream(f_name, ios::in); //Открыть для чтения
+	if (!stream)
+		return -1; //Ошибка открытия файла
+	Pair temp;
+	int i = 0;
+	while (stream >> temp)
+	{
+		cout << temp << "\n";
+		i++;
+	}
+	stream.close();
+	return i;
+}
+//доделать!
+//Удалить все записи большие заданного значения.
+int deletePairs(const char* f_name, int value) {
+	fstream fromStream(f_name, ios::in);
+	fstream inStream("temp", ios::out | ios::trunc);
+	int i = 0;
+	Pair p;
+	while (fromStream >> p) {
+		if (p.get_left() <= value && !fromStream.eof()) {
+			inStream << p;
+			i++;
+		}
+	}
+	fromStream.close();
+	inStream.close();
+	remove(f_name);
+	rename("temp", f_name);
+	return i;
+}
+//доделать!
+//Увеличить все записи с заданным значением на число L.
+int increasePairs(const char* f_name, int currentValue, int addValue)
+{
+	fstream fromStream(f_name, ios::in);
+	fstream inStream("temp", ios::out | ios::trunc);
+	int i = 0;
+	Pair p;
+	while (fromStream >> p) {
+		if (p.get_left() == currentValue && !fromStream.eof()) {
+			p = p + addValue;
+			i++;
+		}
+		inStream << p;
+	}
+	fromStream.close();
+	inStream.close();
+	remove(f_name);
+	rename("temp", f_name);
+	return i;
+}
+//доделать!
+//Добавить K записей после записи с номером N.
+int add_file(const char* f_name, int k, Pair newPair)
+{
+	fstream temp("temp", ios::out); //Открыть для записи
+	fstream stream(f_name, ios::in); //Открыть для чтения
+	if (!stream)
+		return -1; //Ошибка открытия файла
+	Pair p;
+	int i = 0, l = 0;
+	while (stream >> p)
+	{
+		if (stream.eof())
+			break;
+		i++;
+		temp << p;
+		if (i == k)
+		{
+			temp << newPair; //Записать в temp новую запись
+			l++;
+		}
+	}
+	stream.close();
+	temp.close();
+	remove(f_name);
+	rename("temp", f_name);
+	return l; //Количество добавленных
+}
